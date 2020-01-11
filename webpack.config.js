9fcded2bb8d9f263aa11ce0d59cb35011ba432c4
chienvu94 <chienvu@beeketing.net>
2020-01-11 20:54:51 +0700

    Initial project

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");  
+
+module.exports = {
+    // define entry file and output
+    entry: './src/index.js',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'main.js'
+    },
+    // define babel loader
+    module: {
+        rules: [
+           { 
+               test: /\.jsx?$/, 
+               loader: 'babel-loader', 
+               exclude: /node_modules/ 
+           },
+           {  
+               test: /\.css$/,  
+               use: ["style-loader", "css-loader"]  
+           }             
+        ]
+    },
+    plugins: [  
+        new HtmlWebpackPlugin({  
+        	template: "./public/index.html"  
+        })  
+    ]  
+};
