49c83cce9b4095221ba4d84f74fc2679f2813b97
chienvu94 <chienvu@beeketing.net>
2020-01-12 19:34:30 +0700

    2. Using props in function component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react"; 
-    
+
+function Header(props) {
+    return(
+        <p>{props.title}</p>
+    )
+}
+
 class App extends Component {  
     render() {  
         return (  
             <div>  
-                <h1>My React App!</h1>  
+                <Header title="zoro" />  
             </div>  
         );  
     }  
