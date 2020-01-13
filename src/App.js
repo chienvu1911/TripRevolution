ea0286fe8ba8e0b9e9d46c815f012f6a3bd51786
chienvu94 <chienvv@massgenie.com>
2020-01-13 11:18:16 +0700

    5.Using scss

--- src/App.js
+++ src/App.js
@@ -1,82 +1,17 @@
 import React, { Component } from "react"; 
-
-class Header extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isLoading: this.props.isLoading
-        }
-    }
-
-    UNSAFE_componentWillMount() {
-        console.log('componentWillMount Header running after initial and before first render');
-    }
-    
-    componentDidMount() {
-        console.log('componentDidMount Header running after first render and mout component success');
-    }
-
-    render() {
-        console.log('render header');
-        
-        return (  
-            <div>  
-                <p>IsLoading: {this.state.isLoading}</p>
-            </div>  
-        );  
-    } 
-}
+import Navigation from './components/shared/Navigation';
+import Header from './components/shared/Header';
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            isLoading: true
-        }
-    }
-    
-    UNSAFE_componentWillMount() {
-        console.log('componentWillMount running after initial and before first render');
-    }
-    
-    componentDidMount() {
-        console.log('componentDidMount running after first render and mout component success');
-    }
-    
-    shouldComponentUpdate(nextProps, nextState) {
-        console.log('shouldComponentUpdate running first when state change');
-        return true;
-    }
-    
-    UNSAFE_componentWillUpdate(nextProps, nextState) {
-        console.log('componentWillUpdate running after check update by shouldComponentUpdate');
-    }
-    
-    componentDidUpdate(prevProps, prevState) {
-        console.log('componentDidUpdate running after render');
-        console.log('state update successfully');
-    }
-    
-    UNSAFE_componentWillReceiveProps(nextProps) {
-        console.log('componentDidUpdate running first when receive a new props');
-    }
-    
-    componentWillMount() {
-        console.log('You should not call setState() in componentWillUnmount() because the component will never be re-rendered. ');
-    }
+    }   
 
-    onHandleUpdateState = () => {
-        this.setState({
-            isLoading: false
-        })
-    }
-
-    render() {
-        console.log('render app');
+    render() {        
         return (  
             <div>  
-                <Header title="zoro" />
-                <button onClick={this.onHandleUpdateState}>Stop loading</button>  
+                <Navigation />
+                <Header />
             </div>  
         );  
     }  
--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,11 @@ module.exports = {
            {  
                test: /\.css$/,  
                use: ["style-loader", "css-loader"]  
-           }             
+           },
+           {  
+                test: /\.scss$/,  
+                use: ['style-loader', 'css-loader', 'sass-loader']    
+           }                        
         ]
     },
     plugins: [  
