84c59a6ece1770589bd6360584b26d8588802410
chienvu94 <chienvv@massgenie.com>
2020-01-14 17:27:40 +0700

    18. React Redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";  
 import ReactDOM from "react-dom"; 
 import { BrowserRouter as Router } from "react-router-dom"; 
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from "./App.js";
 import 'bootstrap/dist/css/bootstrap.css';    
 import './styles/common.scss';    
 
 const appContainer = document.getElementById('root');
 
-ReactDOM.render(<Router><App /></Router>, appContainer);  
+ReactDOM.render(<Provider>
+    <Router store={store}>
+        <App />
+    </Router>
+</Provider>, appContainer);  
