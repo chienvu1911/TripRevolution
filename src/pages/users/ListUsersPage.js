ac4711ad8f4dce75acc48e0eec86d47ad5af0555
chienvu94 <chienvv@massgenie.com>
2020-01-14 13:44:47 +0700

    13.Using sample data from json file

--- src/pages/users/ListUsersPage.js
+++ src/pages/users/ListUsersPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';  
-import { faSearch, faEye, faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';  
+import { faSearch, faEye, faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+import listUser from '../../data/list-user.json';  
 
 class ListUsersPage extends Component {
     render() {
@@ -24,57 +25,27 @@ class ListUsersPage extends Component {
                             </tr>
                             </thead>
                             <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>Bootstrap 4 CDN and Starter Template</td>
-                                <td>Cristina</td>
-                                <td>2.846</td>
-                                <td>
-                                    <button type="button" className="btn btn-primary">
-                                        <FontAwesomeIcon icon={faEye} />
-                                    </button>
-                                    <button type="button" className="btn btn-success">
-                                        <FontAwesomeIcon icon={faEdit} />
-                                    </button>
-                                    <button type="button" className="btn btn-danger">
-                                        <FontAwesomeIcon icon={faTrash} />
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">2</th>
-                                <td>Bootstrap Grid 4 Tutorial and Examples</td>
-                                <td>Cristina</td>
-                                <td>3.417</td>
-                                <td>
-                                    <button type="button" className="btn btn-primary">
-                                        <FontAwesomeIcon icon={faEye} />
-                                    </button>
-                                    <button type="button" className="btn btn-success">
-                                        <FontAwesomeIcon icon={faEdit} />
-                                    </button>
-                                    <button type="button" className="btn btn-danger">
-                                        <FontAwesomeIcon icon={faTrash} />
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">3</th>
-                                <td>Bootstrap Flexbox Tutorial and Examples</td>
-                                <td>Cristina</td>
-                                <td>1.234</td>
-                                <td>
-                                    <button type="button" className="btn btn-primary">
-                                        <FontAwesomeIcon icon={faEye} />
-                                    </button>
-                                    <button type="button" className="btn btn-success">
-                                        <FontAwesomeIcon icon={faEdit} />
-                                    </button>
-                                    <button type="button" className="btn btn-danger">
-                                        <FontAwesomeIcon icon={faTrash} />
-                                    </button>
-                                </td>
-                            </tr>
+                            {
+                                listUser.map((user, index) => (
+                                    <tr>
+                                        <th>{user.id}</th>
+                                        <td>{user.name}</td>
+                                        <td>{user.phone}</td>
+                                        <td>{user.role}</td>
+                                        <td>
+                                            <button type="button" className="btn btn-primary">
+                                                <FontAwesomeIcon icon={faEye} />
+                                            </button>
+                                            <button type="button" className="btn btn-success">
+                                                <FontAwesomeIcon icon={faEdit} />
+                                            </button>
+                                            <button type="button" className="btn btn-danger">
+                                                <FontAwesomeIcon icon={faTrash} />
+                                            </button>
+                                        </td>
+                                    </tr>
+                                ))
+                            }
                             </tbody>
                         </table>
                     </div>
@@ -88,7 +59,7 @@ class ListUsersPage extends Component {
                     </div>
 
                     <select className="custom-select mb-2" id="inlineFormCustomSelect">
-                        <option selected>Choose...</option>
+                        <option defaultValue>Choose...</option>
                         <option value={1}>One</option>
                         <option value={2}>Two</option>
                         <option value={3}>Three</option>
