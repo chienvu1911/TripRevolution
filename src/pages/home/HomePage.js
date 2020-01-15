1109eac148a9bba2d08efa0b1e7bf6fc8c97f9ce
chienvu94 <chienvv@massgenie.com>
2020-01-15 16:01:44 +0700

    21. Using before to create trangle

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import '../../styles/homepage.scss';
 
 class HomePage extends Component {
     render() {
         return (
-            <div>
+            <div className="home-page">
                 <Link to="/users/1">user chienvu</Link>
+                <div className="arrow-up"></div>
             </div>
         );
     }
